bdc8d9b02ed1a2996ab7b1bd289c1060dd489607
adnanzaih <58091710+adnanzaih@users.noreply.github.com>
2019-11-22 15:58:00 -0700

    added general public license software disclaimer

--- main_DailyHT.py
+++ main_DailyHT.py
@@ -2,30 +2,30 @@
     This script uses the daily W-L phantom
     Hough Transform is used to detect circles
 
-    # Copyright (C) 2019 Adnan Hafeez
-#
-# # This program is a free software: you can redistribute it and/or modify
-# # it under the terms of the GNU Affero General Public License as published
-# # by the Free Software Foundation, either version 3 of the License, or
-# # (at your option) any later version (the "AGPL-3.0+").
-#
-# # This program is distributed in the hope that it will be useful,
-# # but WITHOUT ANY WARRANTY; without even the implied warranty of
-# # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-# # GNU Affero General Public License and the additional terms for more
-# # details.
-#
-# # You should have received a copy of the GNU Affero General Public License
-# # along with this program. If not, see <http://www.gnu.org/licenses/>.
-#
-# # ADDITIONAL TERMS are also included as allowed by Section 7 of the GNU
-# # Affero General Public License. These additional terms are Sections 1, 5,
-# # 6, 7, 8, and 9 from the Apache License, Version 2.0 (the "Apache-2.0")
-# # where all references to the definition "License" are instead defined to
-# # mean the AGPL-3.0+.
-#
-# # You should have received a copy of the Apache-2.0 along with this
-# # program. If not, see <http://www.apache.org/licenses/LICENSE-2.0>.
+    Copyright (C) 2019 Adnan Hafeez
+
+This program is a free software: you can redistribute it and/or modify
+it under the terms of the GNU Affero General Public License as published
+by the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version (the "AGPL-3.0+").
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU Affero General Public License and the additional terms for more
+details.
+
+You should have received a copy of the GNU Affero General Public License
+along with this program. If not, see <http://www.gnu.org/licenses/>.
+
+ADDITIONAL TERMS are also included as allowed by Section 7 of the GNU
+Affero General Public License. These additional terms are Sections 1, 5,
+6, 7, 8, and 9 from the Apache License, Version 2.0 (the "Apache-2.0")
+where all references to the definition "License" are instead defined to
+mean the AGPL-3.0+.
+
+You should have received a copy of the Apache-2.0 along with this
+program. If not, see <http://www.apache.org/licenses/LICENSE-2.0>.
 '''
 
 
@@ -113,7 +113,6 @@ def detectField(input):
     #Done
 
 
-global window
 def combinedPlots(out1,out2):
     distance = math.sqrt(((out1[1] - out2[1]) ** 2) + ((out1[2] - out2[2]) ** 2))
     cv2.imshow("Combined", out1[0]+out2[0])
